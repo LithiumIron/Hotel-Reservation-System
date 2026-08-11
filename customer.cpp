e8d2a0dc3aa271f0c95ed2c96179fd9464f38fe9
moons <moons@LITHIUMIRON>
2026-08-11 17:20:05 +0800

    simplify functions

--- customer.cpp
+++ customer.cpp
@@ -7,33 +7,38 @@ using namespace std;
 
 
 //login
-
 void login(){
     string username,password,password2;
     bool wantBack=false, passwordMatched=false;
     getline(cin,password);
     cout<<"Login"<<endl;
-    do{
-        cout<<"Username [999 to go back]: "<<endl;
-        getline(cin,username);
-        if(username=="999")
-            wantBack=true;
-            
-        while(passwordMatched==false){
-            cout<<"Password: ";
-            getline(cin,password);
-            cout<<"Confirm Password: ";
-            getline(cin,password2);
 
-            if(password!=password2)
-                continue;
-            else {
-                passwordMatched=true;
-                bookingScreen();
-            }
+    cout<<"Username [999 to go back]: "<<endl;
+    getline(cin,username);
+    if(username=="999"){
+        return;
+    }
+
+    while(true){
+        cout<<"Password: ";
+        getline(cin,password);
+
+        cout<<"Confirm Password: ";
+        getline(cin,password2);
+
+        if(password!=password2){
+            cout<<"Error: Both passwords must match\n"<<endl;
+            continue;
         }
+        if(password.length()<8){
+            cout<<"Error: Password must be more than 8 characters\n"<<endl;
+            continue;
+        }
+        break;
+    }
+    bookingScreen();
 
-    }while(wantBack==false);
+
 }
 
 void signup(){
@@ -59,6 +64,7 @@ void homeScreen(){
 
 int main(){
     homeScreen();
+    cout<<"hi test, im back";
     return 0;
 }
 
