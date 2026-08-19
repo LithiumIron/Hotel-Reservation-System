0228348067fb660848e6a156bf514dfb707d9f81
wei1214-ai <khorwei1214@gmail.com>
2026-08-20 01:05:18 +0800

    changes

--- tempBook.cpp
+++ tempBook.cpp
@@ -16,6 +16,10 @@
 
 using namespace std;
 
+// Forward declarations for file I/O functions
+static void saveBookingToFile(const Booking& b);
+static void saveAllBookings(const vector<Booking>& allBookings);
+
 namespace
 {
     const Date SYSTEM_DATE{ 19, 8, 2026 };
